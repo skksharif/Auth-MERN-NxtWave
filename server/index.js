21d2a92fa5c3853a5f6f2454b55e39e3d182ddfb
SKKSharif <115651977+skksharif@users.noreply.github.com>
2025-01-03 23:06:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"))
+app.use(express.static("routes"))
+app.use(express.static("models"))
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
